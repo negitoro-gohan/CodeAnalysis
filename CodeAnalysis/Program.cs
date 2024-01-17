b5528885db0b85ca496d88d21f86f2276178b8f4
negitoro-gohan <44143453+negitoro-gohan@users.noreply.github.com>
2024-01-18 04:28:50 +0900

    プロジェクト ファイルを追加します。

---
+++ CodeAnalysis/Program.cs
@@ -0,0 +1,45 @@
+﻿using CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaslaySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                string directoryPath = args[0];
+                if (Directory.Exists(directoryPath))
+                {
+                    // ディレクトリ内の全.csファイルに対してコードチェックを行う
+                    string[] csFiles = GetCsFiles(directoryPath);
+                    foreach (string csFile in csFiles)
+                    {
+                        SyntaxAnalyzer syntaxAnalysis = new SyntaxAnalyzer();
+                        syntaxAnalysis.GetSelectClauseWithoutOrderClause(csFile);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("指定されたディレクトリは存在しません。");
+                }
+            }
+            else
+            {
+                Console.WriteLine("使用法: プログラム名 <ディレクトリパス>");
+            }
+        }
+        static string[] GetCsFiles(string directoryPath)
+        {
+            // ディレクトリ内のすべての.csファイルを再帰的に取得
+            string[] csFiles = Directory.GetFiles(directoryPath, "*.cs", SearchOption.AllDirectories);
+            return csFiles;
+        }
+    } 
+}
