37da40b5de6165f9b385c9fdb6daadb1c4d73177
negitoro-gohan <44143453+negitoro-gohan@users.noreply.github.com>
2024-01-18 04:28:50 +0900

    プロジェクト ファイルを追加します。

---
+++ CodeAnalysis/SampleSyntasWalker.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+
+namespace CaslaySample
+{
+    public class SampleSyntaxWalker : SyntaxWalker
+    {
+        private SemanticModel semanticModel;
+
+        /// <summary>
+        /// コードの解析を行い、builderに記録します
+        /// </summary>
+        public void Analyze(string code)
+        {
+            // ①単体のソースコードを構文解析します。
+            var tree = CSharpSyntaxTree.ParseText(code);
+
+
+            // 各トークンを見ていく
+            foreach (var token in tree.GetRoot().DescendantTokens())
+            {
+                this.VisitToken(token);
+            }
+        }
+        public void GetSyntax(string sourceCode)
+        {
+            var tree = CSharpSyntaxTree.ParseText(sourceCode);
+            var root = tree.GetRoot();
+
+            var methodNodes = root.DescendantNodes().OfType<MethodDeclarationSyntax>();
+
+            foreach (var methodNode in methodNodes)
+            {
+                var methodName = methodNode.Identifier.Text;
+
+                // メソッド内のすべての Append メソッド呼び出しを取得
+                var appendMethodInvocations = methodNode.DescendantNodes()
+                    .OfType<InvocationExpressionSyntax>()
+                    .Where(IsAppendMethod);
+
+                Console.WriteLine(methodName);
+                bool selectflag;
+                bool orderflag;
+                selectflag = false;
+                orderflag = true;
+                foreach (var appendInvocation in appendMethodInvocations)
+                {
+                    // 引数リスト内に "select" が含まれ、かつ "order" が含まれない場合、メソッド名を出力
+                    var argumentList = appendInvocation.ArgumentList;
+
+                    Console.WriteLine(argumentList.ToString());
+                    if (argumentList.ToString().ToLower().Contains("select"))
+                    {
+                        selectflag = true;
+                    }
+                    if (argumentList.ToString().ToLower().Contains("order"))
+                    {
+                        orderflag = false;
+                    }
+                     
+                }
+                if (selectflag && orderflag)
+                {
+                    Console.WriteLine("match:" + methodName);
+                }
+            }
+
+        }
+        // Appendメソッドかどうかを判定するメソッド
+        static bool IsAppendMethod(InvocationExpressionSyntax invocation)
+        {
+            if (invocation.Expression is MemberAccessExpressionSyntax memberAccess)
+            {
+                var methodName = memberAccess.Name.Identifier.Text;
+                if (methodName == "Append" || methodName == "AppendLine")
+                {
+                    // Appendメソッドの呼び出し
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        /// <summary>
+        /// ④VisitTokenメソッドによるトークンに対する処理
+        /// </summary>
+        /// <param name="token"></param>
+        protected override void VisitToken(SyntaxToken token)
+        {
+            // 詳細を持っているトークンである場合
+            if (token.HasLeadingTrivia)
+            {
+                foreach (var trivia in token.LeadingTrivia)
+                {
+                    VisitTrivia(trivia);
+                }
+            }
+
+            bool isProcessed = false;
+
+            // キーワードであるか
+            if (token.IsKeyword())
+            {
+                Console.WriteLine($"{"Keyword",-30}：{token.ValueText}");
+                isProcessed = true;
+
+            }
+            else
+            {
+                Console.WriteLine($"{token.Kind(),-30}：{token.ValueText}");
+                isProcessed = true;
+            }
+
+            // それ以外の項目
+            if (!isProcessed)
+            {
+                Console.WriteLine($"{"Etc",-30}：{token.ValueText}");
+            }
+
+            if (token.HasTrailingTrivia)
+            {
+                foreach (var trivia in token.TrailingTrivia)
+                {
+                    VisitTrivia(trivia);
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// ⑤VisitTriviaメソッドによるトークンのトリビアに対する処理
+        /// </summary>
+        /// <param name="trivia"></param>
+        protected override void VisitTrivia(SyntaxTrivia trivia)
+        {
+            // 空白と改行の場合は無視
+            if (trivia.Kind() == SyntaxKind.WhitespaceTrivia || trivia.Kind() == SyntaxKind.EndOfLineTrivia)
+            {
+                return;
+            }
+
+            Console.WriteLine($"{trivia.Kind(),-30}：{trivia.ToFullString()}");
+            base.VisitTrivia(trivia);
+        }
+
+    }
+}
---
+++ CodeAnalysis/SyntaxAnalyzer.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAnalysis
+{
+    internal class SyntaxAnalyzer
+    {
+        public void GetSelectClauseWithoutOrderClause(string filePath)
+        {
+
+            var code = File.ReadAllText(filePath);
+
+            var tree = CSharpSyntaxTree.ParseText(code);
+            var root = tree.GetRoot();
+
+            var methodNodes = root.DescendantNodes().OfType<MethodDeclarationSyntax>();
+
+            //メソッド毎に該当箇所があるかチェックを行う
+            foreach (var methodNode in methodNodes)
+            {
+                var methodName = methodNode.Identifier.Text;
+
+                // メソッド内のすべての Append メソッド呼び出しを取得
+                var appendMethodInvocations = methodNode.DescendantNodes()
+                    .OfType<InvocationExpressionSyntax>()
+                    .Where(IsAppendMethod);
+
+                // Append メソッドリスト内に "select" が含まれ、かつ "order" が含まれない場合があるかチェックする
+                bool selectflag;
+                bool orderflag;
+                selectflag = false;
+                orderflag = true;
+                StringBuilder targetSql = new StringBuilder();
+                foreach (var appendInvocation in appendMethodInvocations)
+                {
+
+                    var argumentList = appendInvocation.ArgumentList;
+
+                    targetSql.Append(argumentList.ToString().Replace(@"(""""", "").Replace(@""""")", ""));
+
+                    if (argumentList.ToString().ToLower().Contains("select"))
+                    {
+                        selectflag = true;
+                    }
+                    if (argumentList.ToString().ToLower().Contains("order"))
+                    {
+                        orderflag = false;
+                    }
+
+                }
+
+                // Append メソッドリスト内に "select" が含まれ、かつ "order" が含まれない場合、該当箇所を出力する
+                if (selectflag && orderflag)
+                {
+                    Console.WriteLine("ファイル名:" + filePath);
+                    Console.WriteLine("該当メソッド名:" + methodName);
+                    Console.WriteLine("該当箇所:" + targetSql.ToString()); 
+
+                }
+            }
+
+        }
+        // Appendメソッドかどうかを判定するメソッド
+        static bool IsAppendMethod(InvocationExpressionSyntax invocation)
+        {
+            if (invocation.Expression is MemberAccessExpressionSyntax memberAccess)
+            {
+                var methodName = memberAccess.Name.Identifier.Text;
+                if (methodName == "Append" || methodName == "AppendLine")
+                {
+                    // Appendメソッドの呼び出し
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
